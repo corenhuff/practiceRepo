8197131348ac3c23aedd5b3dd0eb9e182304b024
corenhuff <corenhuff@vt.edu>
2026-06-05 11:35:36 -0400

    work on adding a navbarPage with columns of text

--- shinyApp/first_shinyApp.R
+++ shinyApp/first_shinyApp.R
@@ -4,17 +4,32 @@
 #load libraries
 library(shiny)
 
-ui <- fluidPage(
-  textInput("name", "Enter a name"),
-  textOutput("q")
+ui <- navbarPage("DSPG",
+                 tabPanel("Overview", 
+                          h3("Rural Infrastrucutre and Health Outcomes"), 
+                          textInput("name", "Enter a name"),
+                          textOutput("q"), 
+                          fluidRow(
+                            column(4, 'hey'),
+                            column(4, "i'll be gone today"), 
+                            column(4, "but i'll be back all around the way")
+                            ) 
+                          ),
+                 
+                 tabPanel("Results", 
+                          textOutput("results")
+                          )
 )
 
 server <- function(input, output,
                    session) {
   output$q <- renderText({
-    paste0("What is your favorite hobby, ",
+    paste0("How is your day, ",
            input$name,
            "?")
+  })
+  output$results <- renderText({
+    paste("Results will go here")
   })
   
   
