20ad56b2c8af14cff8f6f30422cf1de88db444b9
corenhuff <corenhuff@vt.edu>
2026-06-05 15:13:19 -0400

    did a little more

--- programs/tidycensus_practice.R
+++ programs/tidycensus_practice.R
@@ -12,27 +12,18 @@ acs_vars <- load_variables(2023,
                            "acs5", 
                             cache = TRUE)
 View(acs_vars)
-#to filter by label and find variable name
-acs_vars |> 
-  filter(grepl("total population", label, ignore.case = TRUE)) |> 
-  View()
-#to check variable 
-acs_vars |>
-  filter(name == "B28002_004")
 
 
-get_acs(geography = "county", 
+data <- get_acs(geography = "county", 
         state = "VA",
         variables = c(
-          broadband = "B28002_004", 
-          pop = "B01003"
+          broadband = "B28002_004",
+          health_insurance = "B27001_001",
+          pop = "B01001_001E"
         ), 
         output = "wide"
 ) |> 
   View()
-
-
-
 
 
 
